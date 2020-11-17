951b8974a7c89011868370d5795e4b3a5775b6c5
Murat <73249227+MuratShn@users.noreply.github.com>
2020-11-17 20:46:50 +0300

    Create List.py

---
+++ List.py
@@ -0,0 +1,27 @@
+urunler = ["Laptop", "Mouse" ,"Keyboard"]
+print(urunler)
+print(type(urunler)) #tür
+urunler.append("Telephone")  #yeni urun eklendı
+print(urunler)
+
+ogrencıler1 = ["ilker","cavit","berkay"]      
+ogrencıler2 = ["kerem","halil","murat"]
+ogrencıler1=ogrencıler2
+ogrencıler2[0]="engin"
+print(ogrencıler1)
+print(ogrencıler2)
+
+
+
+
+
+a=10 #a nın degerı 10                                     #bunlar sayı degerlerı(int)oldugu
+b=20 #b nın degerı 20                                      için bu sekılde 
+a=b  #anın degerı eşitttir bnın değerine diye okunur 
+print(a)
+print(b)
+
+
+
+
+#eleman saymaya 0dan baslanır
